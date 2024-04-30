c07da29ef987f8f0771b605346ab2bd95dbfcd4c
Rayane-Boucheraine <rayaneha08@gmail.com>
2024-04-30 15:53:52 +0100

    change z-index of heading

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
         <h1 className="text-[#062144] text-[40px] font-bold z-50 relative">
           Elites School E-learning platform
         </h1>
-        <Image src={img} className="absolute w-[500px] bottom-0" />
+        <Image src={img} className="absolute w-[550px] bottom-0" />
       </div>
       <div className="flex flex-col gap-[20px] mt-[26px]">
         <h2 className="text-[#062144] text-center text-[28px] not-italic font-bold leading-[24px]">
